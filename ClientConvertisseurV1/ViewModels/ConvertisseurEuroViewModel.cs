c6e715d7bcb2f5024d862b61ff4c3c574a9b0665
Paul Cordellier <paul.cordellier@etu.univ-smb.fr>
2024-01-30 10:31:56 +0100

    Mise en application du patron MVVM

---
+++ ClientConvertisseurV1/ViewModels/ConvertisseurEuroViewModel.cs
@@ -0,0 +1,81 @@
+﻿using ClientConvertisseurV2.Models;
+using ClientConvertisseurV2.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ClientConvertisseurV1.ViewModels
+{
+    public class ConvertisseurEuroViewModel : ObservableObject
+    {
+        private ObservableCollection<Devise> _lesDevises;
+        public ObservableCollection<Devise> LesDevises
+        {
+            get => _lesDevises;
+            set
+            {
+                _lesDevises = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Devise DeviseSelectionnee { get; set; }
+        public double MontantEnEuros { get; set; }
+
+        private double _montantEnDevises;
+        public double MontantEnDevises
+        {
+            get => _montantEnDevises;
+            set
+            {
+                _montantEnDevises = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public IRelayCommand BtnConvertir { get; }
+
+        public ConvertisseurEuroViewModel()
+        {
+            GetDataOnLoadAsync();
+
+            BtnConvertir = new RelayCommand(ActionConvertir);
+        }
+
+        private void ActionConvertir()
+        {
+            MontantEnDevises = MontantEnEuros * DeviseSelectionnee.Taux;
+        }
+
+        private async void GetDataOnLoadAsync()
+        {
+            IService service = new WSService("http://localhost:5272/api/");
+            List<Devise> result = await service.GetDevisesAsync("devises");
+            if (result is null)
+                MessageAsync("API non disponible !", "Erreur"); // Utiliser le code de R401 sur Github pour que ça marche
+            else
+                LesDevises = new ObservableCollection<Devise>(result);
+        }
+
+        private async void MessageAsync(string content, string title)
+        {
+            var messageDialog = new ContentDialog
+            {
+                Title = title,
+                Content = content,
+                CloseButtonText = "OK"
+            };
+
+            //messageDialog.XamlRoot = Content.XamlRoot;
+            await messageDialog.ShowAsync();
+        }
+    }
+}
--- ClientConvertisseurV1/Views/ConvertisseurEuroPage.xaml.cs
+++ ClientConvertisseurV1/Views/ConvertisseurEuroPage.xaml.cs
@@ -1,3 +1,4 @@
+using ClientConvertisseurV1.ViewModels;
 using ClientConvertisseurV2.Models;
 using ClientConvertisseurV2.Services;
 using Microsoft.UI.Xaml;
@@ -19,76 +20,13 @@ using Windows.Foundation.Collections;
 
 namespace ClientConvertisseurV2.Views
 {
-    public sealed partial class ConvertisseurEuroPage : Page, INotifyPropertyChanged
+    public sealed partial class ConvertisseurEuroPage : Page
     {
-        private ObservableCollection<Devise> _lesDevises;
-        public ObservableCollection<Devise> LesDevises
-        {
-            get => _lesDevises;
-            set
-            {
-                _lesDevises = value;
-                OnPropertyChanged(nameof(LesDevises));
-            }
-        }
-
-        public Devise DeviseSelectionnee { get; set; }
-        public double MontantEnEuros { get; set; }
-        private double _montantEnDevises;
-
-        public double MontantEnDevises
-        {
-            get => _montantEnDevises;
-            set
-            {
-                _montantEnDevises = value;
-                OnPropertyChanged(nameof(MontantEnDevises));
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-                handler(this, new PropertyChangedEventArgs(name));
-        }
-
         public ConvertisseurEuroPage()
         {
             InitializeComponent();
-            DataContext = this;
-
-            GetDataOnLoadAsync();
-        }
-
-        private async void GetDataOnLoadAsync()
-        {
-            IService service = new WSService("http://localhost:5272/api/");
-            List<Devise> result = await service.GetDevisesAsync("devises");
-            if (result is null)
-                MessageAsync("API non disponible !", "Erreur"); // Utiliser le code de R401 sur Github pour que ça marche
-            else
-                LesDevises = new ObservableCollection<Devise>(result);
-        }
-
-        private async void MessageAsync(string content, string title)
-        {
-            var messageDialog = new ContentDialog
-            {
-                Title = title,
-                Content = content,
-                CloseButtonText = "OK"
-            };
-
-            messageDialog.XamlRoot = Content.XamlRoot;
-            await messageDialog.ShowAsync();
-        }
-
-        public void btConvertir_Click(object sender, RoutedEventArgs e)
-        {
-            MontantEnDevises = MontantEnEuros * DeviseSelectionnee.Taux;
+            ConvertisseurEuroViewModel convertisseurEuroVM = new ConvertisseurEuroViewModel();
+            DataContext = convertisseurEuroVM;
         }
     }
 }
